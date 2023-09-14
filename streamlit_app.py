d2030493bc0f640f4d4bdf9cf4f4c5f5e22e4e49
danramracingtest <145042202+danramracingtest@users.noreply.github.com>
2023-09-14 11:01:07 -0500

    Update streamlit_app.py
    menu changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+streamlit.write('Omega 3 & Blueberry Oatmeal')
+streamlit.write('Kale, Spinach, & Rocket Smoothie')
+streamlit.write('Hard-Boiled Free-Range Egg')
