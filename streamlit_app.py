1ecb99ff6c3c5002b9ba82429f634b0bc3a4a23f
danramracingtest <145042202+danramracingtest@users.noreply.github.com>
2023-10-04 17:18:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import requests
+
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -24,6 +24,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+import requests
+streamlit.text(fruityvice_response)
 streamlit.text(fruityvice_response.json())
 
 
